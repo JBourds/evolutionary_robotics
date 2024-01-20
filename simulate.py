b1ebca86dd9606b6ed5b66282a036bf41201100e
Jordan Bourdeau <jordan.bourdeau@uvm.edu>
2024-01-20 18:48:19 -0500

    Got env and pybullet set up.

--- simulate.py
+++ simulate.py
@@ -1,1 +1,9 @@
-pass
+"""
+simulate.py
+File to perform physics engine simulations in for Evolutionary Robotics using pybullet.
+
+Author: Jordan Bourdeau
+Date: 1/20/24
+"""
+
+import pybullet as p
