24b50c8c95f9561fc6eaa510e6125df0107e535e
Jordan Bourdeau <jordan.bourdeau@uvm.edu>
2024-01-20 18:51:25 -0500

    Basic simulation setup.

--- simulate.py
+++ simulate.py
@@ -7,7 +7,11 @@ Date: 1/20/24
 """
 
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
-
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(1/60)
+    print(i)
 p.disconnect()
